60ebe56a3334109706a3ece82c15b5f0616631af
Meldgaard99 <100026274+Meldgaard99@users.noreply.github.com>
2024-09-23 21:20:45 +0200

    fixed api naming

--- SearchAPI/Controllers/SearchController.cs
+++ SearchAPI/Controllers/SearchController.cs
@@ -5,7 +5,7 @@ using Shared.Model;
 namespace SearchAPI.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     public class DocumentsController : ControllerBase
     {
         private readonly IDatabase _database;
